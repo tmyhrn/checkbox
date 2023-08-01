19478c5c68c6cd27fc9abb0ac6fd244967341a0e
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-01 05:43:06 +0000

    4-10~12終了

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const isAgreed = document.querySelector('#check');
+
+const btn = document.querySelector('#btn');
+
+isAgreed.addEventListener('change', () => {
+  btn.disabled = !isAgreed.checked;
+});
